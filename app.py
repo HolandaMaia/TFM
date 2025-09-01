b6b887f5d87a6f207e8a4146ae0cf8d5dd9ad3dc
HolandaMaia <142940840+HolandaMaia@users.noreply.github.com>
2025-09-01 22:31:15 +0200

    updated app complete

--- app.py
+++ app.py
@@ -4,15 +4,14 @@ import streamlit as st
 
 homepage = st.Page("Homepage/1_Homepage.py", title="Homepage")
 calculadora = st.Page("Actuarial/2_calculadora.py", title="Actuarial calculator")
-page2 = st.Page("Wallet/2_page2.py", title="Stock")
 page1 = st.Page("Wallet/4_wallet.py", title="Wallet")
-page3 = st.Page("Wallet/3_ativo.py", title="ativo")
-page5 = st.Page("Actuarial/1_apage1.py", title="page1")
+page2 = st.Page("Wallet/3_ativo.py", title="ativo")
+page3 = st.Page("Actuarial/1_apage1.py", title="page1")
 
 pg = st.navigation({
                     "Homepage": [homepage],
-                    "Atuarial": [calculadora,page5],
-                    "Wallet": [page1, page3]
+                    "Atuarial": [calculadora,page3],
+                    "Wallet": [page1, page2]
         })
 
 pg.run()
